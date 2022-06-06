db5dfba70faf259ba8c5bc2cf27d88764d2ccf89
qPexLegendary <terentev.andrey.2002@gmail.com>
2022-06-06 09:14:29 +0700

    Добавлена основа для мастерских

---
+++ src/expo/workbench/Workbench.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace expo\workbench;
+
+use expo\player\PlayerData;
+use expo\player\PlayerManager;
+use pocketmine\player\Player;
+use pocketmine\world\Position;
+
+class Workbench {
+
+    public final function sendToPlayer(Player|PlayerData $player, ?Position $block = null): void {
+        if ($player instanceof Player) $player = PlayerManager::getInstance()->get($player);
+
+        if (!$this->canOpen($player)) return;
+        $this->open($player, $block);
+    }
+
+    protected function canOpen(PlayerData $playerData): bool {
+        return true;
+    }
+
+    protected function open(PlayerData $playerData, ?Position $block): void {
+
+    }
+
+    public function sendCraftList(Player|PlayerData $player): void {
+        if ($player instanceof Player) $player = PlayerManager::getInstance()->get($player);
+
+        //TODO
+    }
+}
